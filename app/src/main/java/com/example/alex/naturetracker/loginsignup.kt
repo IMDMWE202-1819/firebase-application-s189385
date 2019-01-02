782ee1336920c551f292c8c35ca0559cb25c697f
s189385 <s189385@uos.ac.uk>
2019-01-02 18:53:15 +0000

    Adding project files for android studio

---
+++ app/src/main/java/com/example/alex/naturetracker/loginsignup.kt
@@ -0,0 +1,12 @@
+package com.example.alex.naturetracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class loginsignup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginsignup)
+    }
+}
